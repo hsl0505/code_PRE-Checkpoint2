ccfad11667826a78b20c757fa199e02bfad40351
Hyunsung Lim <hslgood@gmail.com>
2019-09-25 17:43:29 +0900

    checkpoint2 complete

--- tenThousandClub/tenThousandClub.js
+++ tenThousandClub/tenThousandClub.js
@@ -1,17 +1,28 @@
 // You are managing a sales team and you are interested is analyzing some data related to their age and total sales.
 // Take a moment to review the following data structure recorded in salesTeam.json for analysis:
 
-if(typeof module === "object" && typeof module.exports === "object") {
+if (typeof module === "object" && typeof module.exports === "object") {
   // OPEN and SEE salesTeam.js
-  var salesTeam = require('../salesTeam.js');
+  var salesTeam = require("../salesTeam.js");
 }
 
-function tenThousandClub(salesTeam){
-  // your code here
+function tenThousandClub(salesTeam) {
+  let arr = [];
+
+  for (let i = 0; i < salesTeam.length; i = i + 1) {
+    if (salesToNumber(salesTeam[i]["sales"]) >= 10000) {
+      arr.push(
+        `${salesTeam[i]["name"]["first"]} ${salesTeam[i]["name"]["last"]}`
+      );
+    }
+  }
+
+  return arr;
 }
 
-function salesToNumber(salesString){
-  //get rid of dollar sign:
+function salesToNumber(salesString) {
+  let deleteDollar = salesString.slice(1);
+  return parseInt(deleteDollar);
 }
 
 var output = tenThousandClub(salesTeam);
