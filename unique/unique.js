b9db53c84895e7cfb280ab62be50b0860f9b8cae
Hyunsung Lim <hslgood@gmail.com>
2019-09-25 17:43:58 +0900

    checkpoint2 complete

--- unique/unique.js
+++ unique/unique.js
@@ -1,6 +1,12 @@
 function unique(array) {
-  // your code here
+  let result = [];
+  for (let i = 0; i < array.length; i = i + 1) {
+    if (!result.includes(array[i])) {
+      result.push(array[i]);
+    }
+  }
+  return result;
 }
 
-var output = unique([1,2,2,4,6,6]); // should be [1,2,4,6]
+var output = unique([1, 2, 2, 4, 6, 6]); // should be [1,2,4,6]
 console.log(output);
