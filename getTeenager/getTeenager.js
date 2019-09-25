aef93c560115209fe6cd414da81af105c1943561
Hyunsung Lim <hslgood@gmail.com>
2019-09-25 17:41:14 +0900

    checkpoint2 complete

--- getTeenager/getTeenager.js
+++ getTeenager/getTeenager.js
@@ -1,13 +1,21 @@
 // You are managing a sales team and you are interested is analyzing some data related to their age and total sales.
 // Take a moment to review the following data structure recorded in salesTeam.json for analysis:
 
-if(typeof module === "object" && typeof module.exports === "object") {
+if (typeof module === "object" && typeof module.exports === "object") {
   // OPEN and SEE salesTeam.js
-  var salesTeam = require('../salesTeam.js');
+  var salesTeam = require("../salesTeam.js");
 }
 
 function getTeenager(salesTeam) {
-  // your code here
+  let arr = [];
+  for (let i = 0; i < salesTeam.length; i = i + 1) {
+    if (salesTeam[i]["age"] < 20 && salesTeam[i]["age"] >= 10) {
+      arr.push(
+        `${salesTeam[i]["name"]["first"]} ${salesTeam[i]["name"]["last"]}`
+      );
+    }
+  }
+  return arr;
 }
 
 var output = getTeenager(salesTeam);
